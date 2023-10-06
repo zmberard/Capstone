e4498f9ade3f5fbbc97b731f609def1a221ba19a
zmberard <74510910+zmberard@users.noreply.github.com>
2023-10-06 18:30:52 +0000

    finished front page UI changes

--- app/src/App.js
+++ app/src/App.js
@@ -4,29 +4,41 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <banner className="App-banner">
-          <p> CS Applications</p>
-          <p> Apply </p>
-      </banner>
-      <header className="App-header">
+
+
+      <ul>
+        <li><a class="active" href="#home">CS Applications</a></li>
+        <li><a href="#news">Apply</a></li>
+      </ul>
+      <div class="container">
+        <div class="jumbotron">
+          <h1>CS Applications</h1>
+          <p>Welcome to the CS Applications System</p>
+        </div>
+      </div>
+      <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <h2>Requirements</h2>
+          <p>In order to be considered for admission to the professional program, a student must have:
+          </p>
+              <ol class="customIndent">
+                <li>Passed all pre-professional program courses with a C or better</li>
+                <li>Achieved at least a 2.3 GPA on all pre-professional courses (including transfer courses)</li>
+              </ol>
+        </div>
+        <div class="col-md-6">
+          <h2> Apply </h2>
+          <p>
+          When you are ready to apply, click here!
+          </p>
+          <button class="button"> Apply! </button>
+        </div>
         
+      </div>
+      
         
-          
-<p>CS Applications</p>
-  
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <form action="../../post" method="post" 
-              className="form">
-          <button type="submit">Connected?</button>
-        </form>
-      </header>
+      </div>
     </div>
   );
 }
