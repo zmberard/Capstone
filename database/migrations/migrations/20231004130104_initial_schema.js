f42bb40a5fe9202d6996b991e03b15aced8ec1e3
nafemage <56566484+nafemage@users.noreply.github.com>
2023-10-06 18:34:28 +0000

    Finalized main migration file

--- database/migrations/migrations/20231004130104_initial_schema.js
+++ database/migrations/migrations/20231004130104_initial_schema.js
@@ -15,6 +15,7 @@ exports.up = function(knex) {
             table.increments("id", { primaryKey: true }).unsigned().notNullable();
             table.integer("user_id").unsigned().defaultTo(0).nullable().unique({indexName:'user_unique_id'});
             table.integer("approved").defaultTo(0).nullable();
+            table.text("application").collate("utf8mb4_unicode_ci");
             table.timestamp("deleted_at").nullable().useNullAsDefault();
             table.timestamp("created_at").nullable().useNullAsDefault();
             table.timestamp("updated_at").nullable().useNullAsDefault();
@@ -44,7 +45,7 @@ exports.up = function(knex) {
         })
         .createTable("settings", function(table){
             table.increments("id", { primaryKey: true }).unsigned().notNullable();
-            table.string("key", 191).collate("utf8mb4_unicode_ci").notNullable().unique();
+            table.string("key", 191).collate("utf8mb4_unicode_ci").notNullable().unique().index();
             table.text("value").collate("utf8mb4_unicode_ci").notNullable();
         })
         .createTable("templates", function(table){
@@ -59,8 +60,8 @@ exports.up = function(knex) {
             table.string("eid", 191).collate("utf8mb4_unicode_ci").notNullable().unique();
             table.string("wid", 191).collate("utf8mb4_unicode_ci").notNullable();
             table.string("remember_token", 100).collate("utf8mb4_unicode_ci").nullable().useNullAsDefault();
-            table.tinyint("admin", 1).notNullable().defaultTo(0);
-            table.tinyint("update_profile", 1).notNullable().defaultTo(0);
+            table.boolean("admin").notNullable().defaultTo(false);
+            table.boolean("update_profile").notNullable().defaultTo(false);
             table.string("first_name", 191).collate("utf8mb4_unicode_ci").notNullable();
             table.string("last_name", 191).collate("utf8mb4_unicode_ci").notNullable();
             table.string("email", 191).collate("utf8mb4_unicode_ci").notNullable();
@@ -76,10 +77,10 @@ exports.up = function(knex) {
  */
 exports.down = function(knex) {
   return knex.schema
-    .dropTable("applications")
-    .dropTable("migrations")
-    .dropTable("sentmails")
-    .dropTable("settings")
-    .dropTable("templates")
-    .dropTable("users");
+    .dropTableIfExists("applications")
+    .dropTableIfExists("migrations")
+    .dropTableIfExists("sentmails")
+    .dropTableIfExists("settings")
+    .dropTableIfExists("templates")
+    .dropTableIfExists("users");
 };
