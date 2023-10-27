e31f7fbcf85925f8f489108fca1985e59cf12825
josh-munda <89316995+josh-munda@users.noreply.github.com>
2023-10-27 14:47:58 +0000

    Notes to self for future reference

--- server/endpoints/apply/process-application.js
+++ server/endpoints/apply/process-application.js
@@ -1,3 +1,7 @@
+// NTS: look at apply controller in the php file given to use
+// and convert the php code into js for each of these functions
+// that apply to the ones in the file
+
 function processApplication(req, res){
     return res.Send('Hello process application')
 }
