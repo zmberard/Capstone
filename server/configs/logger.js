0e735eec0559a357852bfe1159ceea3bc48a6cc6
josh-munda <89316995+josh-munda@users.noreply.github.com>
2023-10-30 14:57:57 +0000

    added to logger and created NODE_ENV environment in server.js

--- server/configs/logger.js
+++ server/configs/logger.js
@@ -2,5 +2,21 @@ const winston = require('winston')
 
 const levels = {
     error: 0,
-    
+    warn: 1,
+    info: 2,
+    http: 3,
+    socket: 4,
+    presence: 5,
+    debug: 6,
 }
+
+const level = () => {
+    const env = process.env.NODE_ENV || 'development'
+    if(env = 'test') return 'error'
+    return env === 'development' ? 'debug' : 'http'
+}
+
+
+
+
+module.exports = logger
