5c96989eaa394784b3f0afb5005c71aded1f4943
josh-munda <89316995+josh-munda@users.noreply.github.com>
2023-10-30 15:55:06 +0000

    updated request-logger

--- server/middleware/request-logger.js
+++ server/middleware/request-logger.js
@@ -6,7 +6,31 @@ const logger = require('../configs/logger')
 // Morgan to use our custom logger instead of the console.log.
 const stream = {
     // Use the http severity
-    write: (message) =>
-      logger.http(message.substring(0, message.lastIndexOf('\n'))),
+    write: (message) => logger.http(message.substring(0, message.lastIndexOf('\n'))),
 }
 
+// Skip all the Morgan http log if the 
+// application is not running in development mode.
+// This method is not really needed here since 
+// we already told to the logger that it should print
+// only warning and error messages in production.
+const skip = () => {
+    const env = process.env.NODE_ENV || "development";
+    return env !== "development";
+  };
+
+// Build the morgan middleware
+const requestLogger = morgan('short', { stream, skip })
+
+morgan.token('remote-user', function (req) {
+    if (req.user_eid) {
+      return 't:' + req.user_eid
+    } else {
+      if (req.session && req.session.user_eid) {
+        return 's:' + req.session.user_eid
+      }
+    }
+    return '-'
+  })
+
+module.exports = requestLogger
