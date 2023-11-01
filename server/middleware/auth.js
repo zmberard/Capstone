a0f37cb8707544cd99783044f8e50e2035dde40b
josh-munda <89316995+josh-munda@users.noreply.github.com>
2023-11-01 15:32:53 +0000

    added models/user + some logic, configs/session, updated middleware/auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,3 +1,4 @@
+// https://github.com/russfeld/officehours-node/blob/main/routes/auth.js
 // Load libraries
 const express = require('express')
 const router = express.Router()
@@ -5,49 +6,102 @@ const jwt = require('jsonwebtoken')
 
 // Load Configurations
 var cas = require('../configs/cas')
-// const token = require('../.../...')
-const requestLogger = require('../middlewares/request-logger') // need to create the request logger
+const requestLogger = require('../middlewares/request-logger')
 
 // Load Models
-// const User = require('../../..')
+// const User = require('../models/user')
 
 // Configure Logging
 router.use(requestLogger)
 
 // Handle Logins
-router.get('/login', async function (req, res, next){
-    if (!req.session.user_id){
-        let eid = ''
-        
-        if(req.query.eid && process.env.FORCE_AUTH === 'true'){
-            // force authentication enabled, use eID from query
-            eid = req.query.eid
+router.get('/login', async function (req, res, next) {
+    if (!req.session.user_id) {
+      let eid = ''
+      if (req.query.eid && process.env.FORCE_AUTH === 'true') {
+        // force authentication enabled, use eID from query
+        eid = req.query.eid
+      } else {
+        // use CAS authentication
+        if (req.session[cas.session_name] === undefined) {
+          // CAS is not authenticated, so redirect
+          req.url = req.originalUrl
+          cas.bounce_redirect(req, res, next)
+          return
         } else {
-            // put below just if and else statements in here
+          // CAS is authenticated, get eID from session
+          eid = req.session[cas.session_name]
         }
-        
-        if(req.session[cas.session_name] === undefined){
-            // CAS is not authenticated, so redirect
-
-            req.url = req.originalUrl
-            cas.bounce_redirect(req, res, next)
-            return
-        } else {
-            // CAS is authenticated, get eID from session
-            eid = req.session[cas.session_name]
-        }
-        if(eid && eid.length != 0){
-            // Find or Create User for eID
-            let user = await User.findOrCreate(eid)
-            // Store User ID in session
-            req.session.user_id = user.user_id
-            req.session.user_eid = eid
-        }
+      }
+      if (eid && eid.length != 0) {
+        // Find or Create User for eID
+        let user = await User.findOrCreate(eid)
+        // Store User ID in session
+        req.session.user_id = user.id
+        req.session.user_eid = eid
+      }
     }
     // Redirect to Homepage
     res.redirect('/')
+  })
+
+// Request New JWT 
+router.get('/token', async function (req, res, next) {
+    if (req.session.user_id) {
+      const token = await User.getToken(req.session.user_id)
+      res.json({
+        token: token,
+      })
+    } else {
+      res.status(401)
+      res.json({ error: 'No Session Established, Please Login' })
+    }
+  })
+
+// Use Refresh Token to Update JWT
+router.post('/token', async function (req, res, next) {
+  if (req.body.refresh_token) {
+    jwt.verify(
+      req.body.refresh_token,
+      process.env.TOKEN_SECRET,
+      async (err, data) => {
+        // console.log('Debugging old refresh tokens')
+        // console.log(err)
+        // console.log(data)
+        if (err) {
+          res.status(401)
+          res.json({ error: 'Error Parsing Token' })
+          return
+        }
+        if (data && data.refresh_token) {
+          // If we receive a verified token, see if it is valid in the database
+          const user = await User.findByRefreshToken(data.refresh_token)
+          if (user != null) {
+            // If it is valid, generate a new token and send
+            const token = await User.getToken(user.id)
+            res.json({
+              token: token,
+            })
+          } else {
+            res.status(401)
+            res.json({
+              error:
+                'Refresh Token Not Found in Database, Session Expired, Please Login',
+            })
+          }
+        } else {
+          res.status(401)
+          res.json({ error: 'Token Data Invalid, Please Login' })
+        }
+      }
+    )
+  } else {
+    res.status(401)
+    res.json({ error: 'Refresh Token Not Found in Request Body' })
+  }
 })
 
+// Handle logout
 router.get('/logout', async function(req, res, next){
     // not sure if needed depending on if we use cookies or tokens
     //if(req.session.user_id){
