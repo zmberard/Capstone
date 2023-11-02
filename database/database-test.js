0eff7bdcad8b41d8f26f4ea4d88c6861b460845e
nafemage <56566484+nafemage@users.noreply.github.com>
2023-11-02 18:04:02 +0000

    added test script to see if scripts work

---
+++ database/database-test.js
@@ -0,0 +1,37 @@
+/* Run migrations and seeds
+ * Wipe db after
+ * Based on starter code here:
+ * https://docs.github.com/en/actions/using-containerized-services/creating-postgresql-service-containers
+ * 
+ * Copyright 2023 under MIT License 
+*/
+
+const { Client } = require('pg');
+
+const pgclient = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'postgres'
+});
+
+pgclient.connect();
+
+const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))'
+const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
+const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com']
+
+pgclient.query(table, (err, res) => {
+    if (err) throw err
+});
+
+pgclient.query(text, values, (err, res) => {
+    if (err) throw err
+});
+
+pgclient.query('SELECT * FROM student', (err, res) => {
+    if (err) throw err
+    console.log(err, res.rows) // Print the data in student table
+    pgclient.end()
+});
