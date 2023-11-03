ddee5cef8e4edbf9ad1b27cf079eab8d6d63e616
nafemage <56566484+nafemage@users.noreply.github.com>
2023-11-03 16:29:57 +0000

    Dev container added and set up. Migrations fixed. Docker compose added and working. Mild code cleanup and restructuring (package file moved into server)

--- server/database/knex.js
+++ server/database/knex.js
@@ -2,5 +2,5 @@
 // Copyright 2023 under MIT License
 
 const environment = process.env.ENVIRONMENT || 'development'
-const config = require('knexfile.js')[environment];
+const config = require('./knexfile.js')[environment];
 module.exports = require('knex')(config);
