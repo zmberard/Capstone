c1120be863002f16a01e223301531cf8e5edeab0
josh-munda <89316995+josh-munda@users.noreply.github.com>
2023-11-21 21:36:09 +0000

    updated models/user, endpoints/login/process-login, and endpoints/login/show-login

--- server/endpoints/login/process-login.js
+++ server/endpoints/login/process-login.js
@@ -1,5 +1,46 @@
-function processLogin(req, res){
-    return res.send('Hello process login')
-}
+// DOUBLE CHECK THE SECRET KEY
 
-module.exports = processLogin
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = retuire('jsonwebtoken')
+const User = require('../models/user');
+
+router.post('/login', async (req, res) => {
+    try{
+        const { eid, password } = req.body;
+        
+        // Validate the both eid and password are provided
+        if(!eid || !password) {
+            return res.status(400).json({ message: 'Username and password are required' });
+        }
+
+        // Find the user by eid in the database
+        const user = await User.findOne({ eid });
+
+        // Check if the user exists
+        if(!user) {
+            return res.status(400).json({ message: 'Invalide eid or password' });
+        }
+
+        // Compare the provided password with the hashed password stored in the database
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        // Check if passwords match
+        if (!passwordMatch){
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+
+        // If everything is valid, create a JWT token for authentication
+        const token = jwt.sign({ userId: user_id }, 'SECRET_KEY', { expiresIn: '1h' }); // need to double check the secret key
+
+        // Send the token to the student
+        res.json({ token });
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Inernal Server Error' });
+    }
+});
+
+module.exports = router;
