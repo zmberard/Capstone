d77b9483e108407331609160609f6d483b5a65f2
josh-munda <89316995+josh-munda@users.noreply.github.com>
2023-11-21 17:07:41 +0000

    updated /endpoints/apply/show-application

--- server/endpoints/apply/show-application.js
+++ server/endpoints/apply/show-application.js
@@ -1,31 +1,43 @@
-function showApplication(req, res){
-    /*
-        step 1 fetch course data from database
-    */
+// load Models
+const Course = require('../models/courseHistory');
 
-    // step 2 calcualte the pre-professional gpa    
-    const preprofessionalGPA = 3.5;
+async function showApplication(req, res){
+    try{
+        // step 1 fetch course data from db
+        const courses = await Course.find({ userId: req.user.id });
 
-    // step 3 populate a application object
-    var form = {
-        eid:"Willie",
-        preprofessionalGPA: preprofessionalGPA,
-        courses:[
-            {
-                name:"CIS115",
-                grade:4,
-                hours:3
-            },
-            {
-                name:"CIS200",
-                grade:3,
-                hours:3
-            }
-        ]
+        // step 2 calculate the pre-professional GPA
+        const preprofessionalGPA = calculateGPA(courses);
+
+        // step 3 populate an application object
+        var form = {
+            eid: req.users.eid,
+            preprofessionalGPA,
+            courses: coursehistory.map(coursehistory => ({
+                name: coursehistory.name,
+                grade: coursehistory.grade,
+                hours: coursehistory.hours
+            }))
+        };
+        // step 4 send the application object to the client
+        res.json(form);
     }
-
-    // Step 4 send the application object to the client
-    res.json(form);
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
 }
 
-module.exports = showApplication
+// helper function to calculate GPA
+function calculateGPA(courses) {
+    if (courses.length === 0) {
+        return 0; // default GPA for when no courses are added
+    }
+
+    const totalPoints = courses.reduce((sum, course) => sum + (course.grade * course.hours), 0);
+    const totalHours = courses.reduce((sum, course) => sum + course.hours, 0);
+
+    return totalPoints / totalHours;
+}
+
+module.exports = showApplication;
