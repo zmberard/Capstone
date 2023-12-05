cc0e3e0bce361f9a321826701da19e34efcc4bc6
zmberard <74510910+zmberard@users.noreply.github.com>
2023-12-05 19:28:56 +0000

    made changes to the css

--- client/app/src/HomePage.js
+++ client/app/src/HomePage.js
@@ -1,45 +1,42 @@
 import React from 'react';
+import "./App.css";
+import { BrowserRouter as Link } from 'react-router-dom';
 
 function HomePage() {
-return (
+return (  
+    <div>
 
-    <div className="App">
-      
+    <ul>
+      <li><Link to="/Home">CS Applications</Link></li>
+      <li><Link to="/Apply">Apply</Link></li>
+    </ul>
 
-      <ul>
-        <li><a class="active" href="#home">CS Applications</a></li>
-        <li><a href="/Apply">Apply</a></li>
-      </ul>
-      <div class="container">
-        <div class="jumbotron">
-          <h1>CS Applications</h1>
-          <p>Welcome to the CS Applications System</p>
-        </div>
-      </div>
-      <div class="container">
-      <div class="row">
-        <div class="col-md-6">
-          <h2>Requirements</h2>
-          <p>In order to be considered for admission to the professional program, a student must have:
-          </p>
-              <ol class="customIndent">
-                <li>Passed all pre-professional program courses with a C or better</li>
-                <li>Achieved at least a 2.3 GPA on all pre-professional courses (including transfer courses)</li>
-              </ol>
-        </div>
-        <div class="col-md-6">
-          <h2> Apply </h2>
-          <p>
-          When you are ready to apply, click here!
-          </p>
-          <button class="button" href="#Apply"> Apply! </button>
-        </div>
-        
-      </div>
-      
-        
+    <div className="container">
+      <div className="jumbotron">
+        <h1>CS Applications</h1>
+        <p>Welcome to the CS Applications System</p>
       </div>
     </div>
+
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <h2>Requirements</h2>
+          <p>In order to be considered for admission to the professional program, a student must have:</p>
+          <ol className="customIndent">
+            <li>Passed all pre-professional program courses with a C or better</li>
+            <li>Achieved at least a 2.3 GPA on all pre-professional courses (including transfer courses)</li>
+          </ol>
+        </div>
+        <div className="col-md-6">
+          <h2>Apply</h2>
+          <p>When you are ready to apply, click here!</p>
+          <Link to="/Apply" className="button">Apply!</Link>
+        </div>
+      </div>
+    </div>
+  
+</div>
   );
 }
 
