dd6509ff9047d56a2cbfaa77cd7e2bb82734e4c8
Jake Houghton <96552435+J-Houghton@users.noreply.github.com>
2024-02-02 04:03:09 +0000

    Fixed Homepage displaying in all tabs, andded routes

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import ApplicationForm from './ApplicationForm.js';
 import ProfilesForm from './ProfilesForm.js';
+import HomePage from './HomePage.js';
 import Header from './Header';  
 import Footer from './Footer'; 
 
@@ -11,34 +12,12 @@ function App() {
       <div className="App">
         <Header/>
         <Routes>
-          {/* not sure why home is not needed here*/}
+           <Route path="/" element={<HomePage />} />
+          <Route path="/Home" element={<HomePage />} />
           <Route path="/Apply" element={<ApplicationForm />} />
           <Route path="/Profile" element={<ProfilesForm />} />
         </Routes>
-          <div className="container">
-            <div className="jumbotron">
-              <h1>CS Applications</h1>
-              <p>Welcome to the CS Applications System</p>
-            </div>
-          </div>
-
-          <div className="container">
-            <div className="row">
-              <div className="col-md-6">
-                <h2>Requirements</h2>
-                <p>In order to be considered for admission to the professional program, a student must have:</p>
-                <ol className="customIndent">
-                  <li>Passed all pre-professional program courses with a C or better</li>
-                  <li>Achieved at least a 2.3 GPA on all pre-professional courses (including transfer courses)</li>
-                </ol>
-              </div>
-              <div className="col-md-6">
-                <h2>Apply</h2>
-                <p>When you are ready to apply, click here!</p>
-                <Link to="/Apply" className="button">Apply!</Link>
-              </div>
-            </div>
-          </div>
+          
           <Footer/>
       </div>
     </Router>
