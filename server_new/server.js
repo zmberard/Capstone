ebe3cec143d7c175f2d408896b03d828a308a498
nafemage <56566484+nafemage@users.noreply.github.com>
2024-03-20 18:55:51 +0000

    Updates to add auth and login req

--- server_new/server.js
+++ server_new/server.js
@@ -18,11 +18,20 @@ const userRoutes = require('./routes/userRoutes');
 const courseRoutes = require('./routes/courseRoutes');
 const applicationRoutes = require('./routes/applicationRoutes');
 const dataRoutes = require('./routes/dataRoutes');
+const auth = require('./routes/auth');
 
 app.use('/api', userRoutes);
 app.use('/api', courseRoutes);
 app.use('/api', applicationRoutes);
 app.use('/api', dataRoutes);
+
+// Since all our rotes are served with the `/api` prefix,
+// we might as well set up a subrouter using it:
+const router = express.Router();
+app.use('/api', router);
+
+// Use CAS-based authentication to log users in
+router.use(auth);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
