725b5508a3ad7696be507eb52354962d286f8b11
Jake Houghton <jakehoughton@ksu.edu>
2024-04-24 13:35:31 -0500

    cleaned up server

--- server/configs/default.js
+++
@@ -1,8 +0,0 @@
-module.exports ={
-    database: {
-        enabled: true, // true if the database is enabled, false otherwise
-    },
-    server: {
-        port: 8080,
-    },
-}
--- server/database/knex.js
+++
@@ -1,6 +0,0 @@
-// Knex config file
-// Copyright 2023 under MIT License
-
-const environment = process.env.ENVIRONMENT || 'development'
-const config = require('./knexfile.js')[environment];
-module.exports = require('knex')(config);
